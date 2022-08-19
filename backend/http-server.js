8c5da951a4add59e36fde33e6a043bfb7fab25d9
andiub97 <43032657+andiub97@users.noreply.github.com>
2022-08-19 10:48:09 +0200

    upd http-server

--- backend/http-server.js
+++ backend/http-server.js
@@ -1,15 +1,26 @@
 const http = require('http');
 
+const portHttp = 8081
+const host = '127.0.0.1'
+const http_server = express()
+
+// app.use(express.static(__dirname + "/public", {
+//     index: false,  // no index
+//     immutable: true,  // immutable static files
+//     cacheControl: true, // always in cache
+//     maxAge: "30d" // death time
+// }));
+
 const server = http.createServer((req, res) => {
     let data = '';
     req.on('data', chunk => {
         data += chunk;
     });
     req.on('end', () => {
-        console.log(data); // 'Buy the milk'
+        console.log(data);
         res.end();
     });
 });
 
 
-server.listen(8080);
+server.listen(8081);
