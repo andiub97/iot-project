ac79786588c624673e8419bef4444fa67d52ff65
andiub97 <43032657+andiub97@users.noreply.github.com>
2022-08-24 19:07:42 +0200

    influx sends data

--- influxdb/InfluxManager.js
+++ influxdb/InfluxManager.js
@@ -13,7 +13,7 @@ class InfluxManager {
 
     writeApi(clientId, gps, bucket, value) {
         const writeApi = this.client.getWriteApi(this.org, bucket)
-        writeApi.useDefaultTags({ prediction: "no", host: clientId.toString(), lat: gps.lat.toString(), lng: gps.lng.toString() })
+        writeApi.useDefaultTags({ prediction: "no", host, lat: gps.lat.toString(), lng: gps.lng.toString() })
         var point = new Point('val')
         if (bucket == undefined || value == null) {
             return false;
